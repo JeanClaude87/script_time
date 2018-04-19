73a63424aed47bed9e179488b50adf882b39e0b9
Piero Claude <naldesi@bo.infn.it>
2018-04-19 17:19:33 +0200

    uga

--- AA_analizza.py
+++ AA_analizza.py
@@ -13,20 +13,27 @@ LOCAL = os.path.abspath('../')
 
 #....................dopo avere creato cartelle e sotto cartelle
 #....................restituisce in un vettori le varie possibili combinazioni L,D
-path_file = LOCAL+'/DATI*/*/*/'
-directory = sorted(glob.glob(path_file))	
-namesLD = ff.folder_crea(LOCAL,directory)
-
 
 comm = MPI.COMM_WORLD
 size = comm.Get_size()
 rank = comm.Get_rank()
 
-for kk,name in enumerate(namesLD):
-	if kk%size!=rank: continue
+if rank == 0:
+	path_file = LOCAL+'/DATI*/*/*/'
+	directory = sorted(glob.glob(path_file))	
+	namesLD = ff.folder_crea(LOCAL,directory)
+
+
+namesLD = comm.scatter(data, root=0)
+print 'rank',rank,'has data:',data
+
+
+
+#for kk,name in enumerate(namesLD):
+#	if kk%size!=rank: continue
 	
 	#ff.Io_fascio_tuto(name,LOCAL)
-	uga = 1
+#	uga = 1
 
 print('YO', name)
 
