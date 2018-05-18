8fc20efb388917c7427f115d9c888bf20a9e6229
Piero Claude <naldesi@bo.infn.it>
2018-05-18 14:12:27 +0200

    uga

--- DMRG_ST/dmrgtev-12.5/version.h
+++ DMRG_ST/dmrgtev-12.5/version.h
@@ -1,13 +1,13 @@
-/*      version.h                       (C) 2016 Fabio Ortolani fbo 180517
+/*      version.h                       (C) 2016 Fabio Ortolani fbo 180518
         ==================================================================  */
 #ifndef VERSION_H
 #define VERSION_H
 /*  Automatic generated file. Don't edit!  */
 #define NAME      "dmrgtev"
 #define VERSION   "12.5"
-#define COPYRIGHT "(C) 2016 Fabio Ortolani fbo 180517"
-#define HOSTTYPE  ""
-#define OSTYPE    ""
-#define MACHTYPE  ""
+#define COPYRIGHT "(C) 2016 Fabio Ortolani fbo 180518"
+#define HOSTTYPE  "x86_64"
+#define OSTYPE    "darwin17"
+#define MACHTYPE  "x86_64-apple-darwin17"
 #endif /* VERSION_H */
 /* =======================================================================  */
