58e1c63dd4c24db52714ba6a4261805641dce8e7
Piero Naldesi <naldesi@bo.infn.it>
2018-05-21 18:56:24 +0200

    uga

--- DMRG_ST/dmrgtev-12.5/version.h
+++ DMRG_ST/dmrgtev-12.5/version.h
@@ -1,13 +1,13 @@
-/*      version.h                       (C) 2016 Fabio Ortolani fbo 171109
+/*      version.h                       (C) 2016 Fabio Ortolani fbo 180521
         ==================================================================  */
 #ifndef VERSION_H
 #define VERSION_H
 /*  Automatic generated file. Don't edit!  */
 #define NAME      "dmrgtev"
 #define VERSION   "12.5"
-#define COPYRIGHT "(C) 2016 Fabio Ortolani fbo 171109"
-#define HOSTTYPE  "x86_64"
-#define OSTYPE    "darwin17"
-#define MACHTYPE  "x86_64-apple-darwin17"
+#define COPYRIGHT "(C) 2016 Fabio Ortolani fbo 180521"
+#define HOSTTYPE  ""
+#define OSTYPE    ""
+#define MACHTYPE  ""
 #endif /* VERSION_H */
 /* =======================================================================  */
--- DMRG_ST/dmrgtev-12.5/version.hh
+++ DMRG_ST/dmrgtev-12.5/version.hh
@@ -1,13 +1,13 @@
-//      version.hh                      (C) 2016 Fabio Ortolani fbo 171109
+//      version.hh                      (C) 2016 Fabio Ortolani fbo 180521
 //      ==================================================================
 #ifndef VERSION_HH
 #define VERSION_HH
 //      Automatic generated file. Don't edit!
 #define NAME      "dmrgtev"
 #define VERSION   "12.5"
-#define COPYRIGHT "(C) 2016 Fabio Ortolani fbo 171109"
-#define HOSTTYPE  "x86_64"
-#define OSTYPE    "darwin17"
-#define MACHTYPE  "x86_64-apple-darwin17"
+#define COPYRIGHT "(C) 2016 Fabio Ortolani fbo 180521"
+#define HOSTTYPE  ""
+#define OSTYPE    ""
+#define MACHTYPE  ""
 #endif // VERSION_HH
 //========================================================================
