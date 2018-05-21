422ddd1860ae863fa13fdd820bdc9cf9772bfc92
Piero Naldesi <naldesi@bo.infn.it>
2018-05-21 18:12:28 +0200

    uga

--- DMRG_ST/dmrgtev-12.5/version.hh
+++ DMRG_ST/dmrgtev-12.5/version.hh
@@ -6,8 +6,8 @@
 #define NAME      "dmrgtev"
 #define VERSION   "12.5"
 #define COPYRIGHT "(C) 2016 Fabio Ortolani fbo 180518"
-#define HOSTTYPE  "x86_64"
-#define OSTYPE    "darwin17"
-#define MACHTYPE  "x86_64-apple-darwin17"
+#define HOSTTYPE  ""
+#define OSTYPE    ""
+#define MACHTYPE  ""
 #endif // VERSION_HH
 //========================================================================
